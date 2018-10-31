a74fe40b49f8e9c399e815339d7a914fdc15b8dc
Euan Murray <euan.murray@itftennis.com>
2018-10-31 17:15:15 +0000

    react-not finished

---
+++ Itf.Cms.Poc/scripts/React/Components/Carousel.js
@@ -0,0 +1,18 @@
+﻿import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+//ReactDOM.render(
+//    <h1>Carousel</h1>,
+//    document.getElementById('carousel')
+//    );
+
+class Carousel extends Component {
+    render() {
+        return (
+            <div>Hello world - {this.props.worked}</div>
+);
+}
+}
+
+//var root = document.getElementById('carousel');
+//ReactDOM.render(<Carousel worked = { window.myVar }/>, root)
---
+++ Itf.Cms.Poc/scripts/React/webpack.config.js
@@ -0,0 +1,48 @@
+﻿module.exports = [
+    {
+        context: __dirname,
+        entry: "./app.js",
+        output: {
+            path: __dirname + "/dist",
+            filename: "app-bundle.js"
+        },
+        watch: true,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['babel-preset-env', 'babel-preset-react']
+                        }
+                    }
+                }
+            ]
+        }
+    },
+    {
+    context: __dirname,
+    entry: "./components/Carousel.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "carsousel-bundle.js"
+    },
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env', 'babel-preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
+]
